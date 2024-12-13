72fd1abe3d7fbfc74da609d80ecdfd1afc02ec3e
SchlagMartin <schlagm0@gmail.com>
2024-12-13 08:35:08 +0100

    Initial commit

---
+++ database/migrations/2024_12_13_072017_gifts.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gifts', function (Blueprint $table) {
+            $table->id();
+            $table->string('gift_name');
+            $table->string('email');
+            $table->unsignedBigInteger('gift_type_id');
+            $table->foreign('gift_type_id')->references('id')->on('gift_types')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gifts');
+    }
+};
